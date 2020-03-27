ffed1fb0c828de6200ccf9e94e82b41aa7f72c04
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-03-27 15:27:06 -0300

    Stopped at 48:29

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {FiLogIn} from 'react-icons/fi';
 
 import './styles.css';
 
+import logo from '../../assets/logo.svg'
+import heroesImage from '../../assets/heroes.png'
+
 export default function Logon() {
   return (
-    <h1>Logon</h1>
+    <div className="logon-container">
+      <section className="form">
+        <img src={logo} alt="Be The Hero" />
+
+        <form>
+          <h1>Faça seu Logon</h1>
+          <input type="text" placeholder="Sua ID"/>
+          <button className="button" type="submit">Entrar</button>
+
+          <a href="/register">
+            <FiLogIn size={16} color="#e02041" />
+            Não tenho cadastro
+          </a>
+        </form>
+      </section>
+      <img src={heroesImage} alt="Heroes" />
+    </div>
   );
 }
