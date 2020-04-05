3bf49bfd2f86f5e4b2baf0526b0de06a0f422d0a
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-04-05 18:16:32 -0300

    Stopped at 16:00 of the last video

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const cors = require('cors');
 
+const {errors} = require('celebrate');
+
 const routes = require('./routes');
 
 const app = express();
@@ -11,5 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 app.listen(3333);
